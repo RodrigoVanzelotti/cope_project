1f68a664518c8e108bf8b88b1501c6206ead219d
RodrigoVanzelotti <rodrigovanzelotti@gmail.com>
2023-08-14 10:13:44 -0300

    added boleto_detector

---
+++ logger.py
@@ -0,0 +1,17 @@
+import logging
+
+class Logger:
+    def __init__(self):
+        pass
+
+
+logging.basicConfig(leve=logging.WARNING)
+
+logging.debug("aa")
+logging.info("aa")
+logging.warning("aa")
+logging.critical("aa")
+
+
+if __name__ == '__main__':
+    pass
